ab0bf8cfdfe4f5a9f7d5b9115f7072d016484eac
treblot <treblotnad@gmail.com>
2023-11-28 15:54:08 -0600

    added coloring by time of day to work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 const dayLength = 9;
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -9,12 +6,6 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -28,10 +19,16 @@ $(function () {
 function renderCalendar() {
   for (let i = 0; i < dayLength; i++) {
     var hour = i + dayLength;
-    var hourStatus = "";
+    var hourStatus = "past";
+    if (dayjs().hour() > hour) {
+      hourStatus = "future";
+    }
+    if (dayjs().hour() == hour) {
+      hourStatus = "present";
+    }
     var hourToBeAdded = $("<div>")
       .attr("id", "hour-" + i)
-      .addClass("row time-block past");
+      .addClass("row time-block " + hourStatus);
 
     hourToBeAdded.append(
       $("<div>" + dayjs().hour(hour).format("hA") + "</div>").addClass(
